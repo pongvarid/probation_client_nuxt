1bea92aeb537789c163cf24f120198ae03d9323f
pongvarid <pongvarid@gmail.com>
2022-11-26 11:06:35 +0700

    Updates

---
+++ _nuxt/1088b48.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{788:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},c=t(59),component=Object(c.a)(l,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
