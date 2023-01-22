29bcd1facc3ba7c8dce753461e654fcb0138020a
pongvarid <pongvarid@gmail.com>
2023-01-23 00:57:23 +0700

    add title

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,8 +13,8 @@ export default {
  
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - ui-orange',
-    title: 'ui-orange',
+    titleTemplate: 'ระบบค้นหางาน',
+    title: 'ระบบค้นหางาน',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
